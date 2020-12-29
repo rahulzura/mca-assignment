08c65849ce547f9c58d056812ba2dbcaa3ac18a9
Rahul Kumar <rahulkay3@gmail.com>
2020-12-29 21:46:47 +0530

    add event listerner through js

--- sem-1/mcsl-016/uni-web/js/register.js
+++ sem-1/mcsl-016/uni-web/js/register.js
@@ -1,9 +1,15 @@
+window.onload = () => {
+    const submitBtn = document.querySelector("#register-btn");
+    submitBtn.addEventListener('click', onSubmitHandler);
+}
+
 const onSubmitHandler = (e) => {
-    const nameEl = document.querySelector("name-input");
-    const emailEl = document.querySelector("email-input");
-    const phoneEl = document.querySelector("phone-input");
-    const qualificationEl = document.querySelector("qualification-input");
-    const programEl = document.querySelector("program-input");
+    e.preventDefault();
+    const nameEl = document.querySelector("#name-input");
+    const emailEl = document.querySelector("#email-input");
+    const phoneEl = document.querySelector("#phone-input");
+    const qualificationEl = document.querySelector("#qualification-input");
+    const programEl = document.querySelector("#program-input");
 
     console.log(nameEl.value, emailEl.value, phoneEl.value, qualificationEl.value, programEl.value);
 }
